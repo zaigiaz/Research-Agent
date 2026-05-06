83d9f283073014d934aa5bf44d1e1f19164f0e63
zaigiaz <shayne.tman@gmail.com>
2026-05-06 13:54:57 -0500

    basic response testing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,23 @@
-import llama_cpp
+from llama_cpp import Llama
 
-print("hello")
+# TODO :: Get Basic Text and Response Format in easy chat format type of way
+# TODO :: Create Toml or yaml object for config file
+# TODO :: Follow Tutorial for tool-calling etc.
+
+# Basic pattern to initialize a model
+llm = Llama(
+      model_path="/home/zaigiaz/third_party/ai_models/Qwen/Qwen3.5-9B-Q6_K.gguf",
+      # n_gpu_layers=-1, # Uncomment to use GPU acceleration
+      # seed=1337, # Uncomment to set a specific seed
+      n_ctx=9056, # Uncomment to increase the context window
+)
+
+# Basic Output Object
+output = llm(
+      "Q: Name the planets in the solar system? A: ", # Prompt
+      max_tokens=256, # Generate up to 32 tokens, set to None to generate up to the end of the context window
+      stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+      echo=True # Echo the prompt back in the output
+) # Generate a completion, can also call create_completion
+
+print(output)
