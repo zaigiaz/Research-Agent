3679de56cfa8788e3a191d094ab25941e8e972af
zaigiaz <shayne.tman@gmail.com>
2026-05-08 09:28:30 -0500

    functions, argparsing, idea iteration

---
+++ makefile
@@ -0,0 +1,7 @@
+# Main Makefile for project
+
+target:
+	python3 src/main.py
+
+test:
+	python3 -m py_compile src/main.py
--- src/main.py
+++ src/main.py
@@ -1,23 +1,37 @@
 from llama_cpp import Llama
 
-# TODO :: Get Basic Text and Response Format in easy chat format type of way
-# TODO :: Create Toml or yaml object for config file
-# TODO :: Follow Tutorial for tool-calling etc.
+# TODO :: Get Basic Text and Response Format in easy chat format kind of way
+# TODO :: Create JSON config file
+# TODO :: Arg-parsing for prompt and each kind of search backend
+# TODO :: Follow Tutorial for tool-calling etc. (pydantic)
 
-# Basic pattern to initialize a model
-llm = Llama(
-      model_path="/home/zaigiaz/third_party/ai_models/Qwen/Qwen3.5-9B-Q6_K.gguf",
-      # n_gpu_layers=-1, # Uncomment to use GPU acceleration
-      # seed=1337, # Uncomment to set a specific seed
-      n_ctx=9056, # Uncomment to increase the context window
-)
+# Init basic Llama model
+def init_model() -> Llama:
+    # Basic pattern to initialize a model
+    llm = Llama(
+        model_path="/home/zaigiaz/third_party/ai_models/Qwen/Qwen3.5-9B-Q6_K.gguf",
+        # n_gpu_layers=-1, 
+        # seed=1337, 
+        n_ctx=9056,
+    )
+    return llm
+
+
+def arg_parsing() -> dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--ctx", type=int, required=False)
+    parser.add_argument("--max-tokens", type=int, required=False)
+
+    return vars(parser.parse_args())
+
+llm = init_model()
 
 # Basic Output Object
 output = llm(
-      "Q: Name the planets in the solar system? A: ", # Prompt
-      max_tokens=256, # Generate up to 32 tokens, set to None to generate up to the end of the context window
-      stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
-      echo=True # Echo the prompt back in the output
-) # Generate a completion, can also call create_completion
+      "Question: Name the planets in the solar system?",
+      max_tokens=24,
+      echo=False 
+)
 
-print(output)
+print(output['choices'][0]["text"])
