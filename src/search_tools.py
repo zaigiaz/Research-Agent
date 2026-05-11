33b62e3400f665e81da6fea6f4602c7a6d2aa0eb
zaigiaz <shayne.tman@gmail.com>
2026-05-11 09:21:40 -0500

    added ddgs api for metasearch, can extract and search through one package

--- src/search_tools.py
+++ src/search_tools.py
@@ -1,10 +1,18 @@
-# file where we define each tool call we can make and the web-search API's that we can call
-
 import pydantic
+from ddgs import DDGS
 from typing import Any
 
-# TODO :: Get basic duckduckgo api implemented with requests
-# TODO :: think about other search backends and integrate
+# TODO :: arxiv or wikipedia api for more narrowed search
 
-print("Hello")
+results = DDGS().text("butterfly wing patterns 2026", max_results=5)
 
+for i,v in enumerate(results):
+   print("--------------------")
+   print(results[i]['title'])
+   print(results[i]['href'])
+   print(results[i]['body'])
+   print("--------------------")
+
+
+# result = DDGS().extract("https://en.wikipedia.org/wiki/Modularity_(networks)", fmt="text_rich")
+# print(result['content'])
--- src/tools.py
+++ src/tools.py
@@ -1,2 +1,4 @@
 # Regular tools like reading and writing to a file
 
+import pydantic
+import json
