6eee92e07867ac497f7e5767d3a03dd804c11a18
zaigiaz <shayne.tman@gmail.com>
2026-05-16 19:06:22 -0500

    small update

--- src/tool_proxy.py
+++ src/tool_proxy.py
@@ -1,7 +1,7 @@
 import pydantic
 import os
 
-# TODO :: create counters with itertools for how many tool-calls for each agent / sub-agent
+# TODO :: create counters for each tool-call on each agent
 # TODO :: create checks for staying inside task_dir
 # TODO :: create response messages back to AI or user in-case we run out or prematurely end task
 
